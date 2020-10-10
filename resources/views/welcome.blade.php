df2fef66dd1729ab8ed02c5b529ae45b6f4dff6f
Mr.Eagle <mreagle.design@gmail.com>
2020-10-09 19:03:59 -0500

    front tweak

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>SCAPI</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -65,7 +65,7 @@
     </head>
     <body>
         <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
+            {{-- @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
                         <a href="{{ url('/home') }}">Home</a>
@@ -77,22 +77,19 @@
                         @endif
                     @endauth
                 </div>
-            @endif
+            @endif --}}
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Simpol-Commerce API
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="#" target="_blank" rel="nofollow">Docs</a>
+                    <a href="#" target="_blank" rel="nofollow">Noticias</a>
+                    <a href="#" target="_blank" rel="nofollow">Blog</a>
+                    <a href="https://scapp2.netlify.app" target="_blank" rel="nofollow">SCAPP</a>
+                    <a href="https://github.com/Mr3agle/sc_api" target="_blank" rel="nofollow">GitHub</a>
                 </div>
             </div>
         </div>
